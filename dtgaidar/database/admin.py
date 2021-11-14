962e16db6fbf02e5828eb5a686fa3a5ddccbdf51
Riox <azdemonic@gmail.com>
2021-11-14 17:18:14 +0300

    1

--- dtgaidar/database/admin.py
+++ dtgaidar/database/admin.py
@@ -3,10 +3,11 @@ from .models import Base
 from import_export import resources
 from import_export.admin import ImportExportModelAdmin
 
+
 class BookResource(resources.ModelResource):
     class Meta:
         model = Base
-
+ 
 class BookAdmin(ImportExportModelAdmin):
     resource_class = BookResource
 
